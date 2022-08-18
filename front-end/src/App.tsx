3dce16c1007024a190543d5c9e7217f15b1983b4
Agam Singh <singh.agam0009@gmail.com>
2022-08-18 11:22:36 +1000

    added selector with blurb (with mockdata)

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import LandingPage from './LandingPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LandingPage />
     </div>
   );
 }
