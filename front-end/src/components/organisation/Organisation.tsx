8fb9245f8330eb454603897cbbab0e4fa619d6d0
103620582 <103620582@student.swin.edu.au>
2022-09-06 12:03:25 +1000

    :children_crossing: org info page done

---
+++ front-end/src/components/organisation/Organisation.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+
+import { db } from '../../config/firebase'
+import { doc, getDoc } from 'firebase/firestore'
+import DonorForm from '../donor-form/DonorForm'
+
+import './Organisation.css'
+import { Button, Card } from 'react-bootstrap'
+
+function Organisation() {
+  let params = useParams()
+  const [org, setOrg] = useState<any>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const orgId = params.orgId || ''
+      const docRef = doc(db, 'Organisations', orgId)
+      const docSnap = await getDoc(docRef)
+      setOrg(docSnap.data())
+    }
+    fetchData().catch(console.error)
+  }, [params.orgId])
+
+  return (
+    //   these lines set up the format of the page
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: 20,
+      }}
+    >
+      <Card
+        className="orgInfo">
+        <h6 style={{ textAlign: 'left', margin: '7px' }}>
+          YOU'RE DONATING TOWARDS Edable SUPPORTING
+        </h6>
+        <Card.Title>{org.name}</Card.Title>
+        <Card.Img variant="top" src={org.img} alt={`${org.name}`} />
+        <Card.Body>
+          <Card.Text>{org.description}</Card.Text>
+        </Card.Body>
+        <Link to="/">
+          <Button variant="warning">Go back</Button>
+        </Link>
+      </Card>
+
+      <DonorForm />
+
+    </div>
+  )
+}
+
+export default Organisation
