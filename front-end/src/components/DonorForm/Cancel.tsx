a5509a421dace9baede2e7d7ea639247843986f5
tn-guyen <101620519@student.swin.edu.au>
2022-09-08 14:35:51 +1000

    styling success and cancel pages

--- front-end/src/components/DonorForm/Cancel.tsx
+++ front-end/src/components/DonorForm/Cancel.tsx
@@ -1,28 +1,34 @@
-import { Button } from "react-bootstrap";
+import { Button, Image } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 
 function Cancel() {
   let params = useParams();
   return (
     //   these lines set up the format of the page
-    <>
-      <h3>
-        We couldn't complete your transaction. Would you like to
-        <div>
+    <div style={{ textAlign: "center", width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <div>
+      <Image src='https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Fmobile%2F000%2F028%2F692%2Fcat.jpg' style={{width:"300px", height:"300px"}} roundedCircle></Image>
+        <h3 style={{ textAlign: "center", paddingTop:"30px"}}>
+          Sorry, we couldn't complete your transaction. 
+        </h3>
+        <h3 style={{ textAlign: "center", paddingTop: "30px"}}>
+          Would you like to...
+        </h3>
+        <h3 style={{textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center" }}>
           <Link to={`../../organisation/${params.orgId}`}>
-            <Button>
+            <Button variant="warning" style={{ textDecoration: "none", color: "black", fontSize: "30px" }}>
               Try Again
             </Button>
           </Link>
-          or
+          &nbsp;or&nbsp;
           <Link to="/">
-            <Button>
-              return to Donation page
+            <Button variant="warning" style={{ textDecoration: "none", color: "black", fontSize: "30px" }}>
+              Return to Donation page
             </Button>
           </Link>
-        </div>
-      </h3>
-    </>
+        </h3>
+      </div>
+    </div>
   );
 }
 
--- front-end/src/components/DonorForm/Success.tsx
+++ front-end/src/components/DonorForm/Success.tsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
+import { Image, Button } from "react-bootstrap";
 
 function Success() {
     return (
         //   these lines set up the format of the page
         <div>
-            <h1>
-                Thank you for your donation.
-            </h1>
-            <div>
-                <Link to="/">Home</Link>
+            <Image src= "https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1175550351.jpg" style={{height:"40vh",width:"100vw", objectFit:"cover"}}></Image>
+            <div style={{ height: "60vh", width: "100vw", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <div>
+                    <h1 style={{ textAlign: "center", fontSize: "80px" }}>
+                        Thank You For Donating!
+                    </h1>
+                    <h2 style={{ textAlign: "center", fontSize: "35px", padding: "40px" }}>
+                        An email with your receipt will be sent to you.
+                    </h2>
+                    <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                        <Link to={'/'}>
+                            <Button variant="warning" style={{ textDecoration: "none", color: "black", fontSize: "30px" }}>
+                                <i>~click here to donate again~</i>
+                            </Button>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
