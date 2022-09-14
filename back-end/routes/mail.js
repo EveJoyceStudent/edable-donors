0da1c646b7bcf7885f5a0b625d04c2ba6e1e663c
Agam-S <singh.agam0009@gmail.com>
2022-09-14 19:47:36 +1000

    mail bot added, sends email so far

---
+++ back-end/routes/mail.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+const nodemailer = require("nodemailer");
+const handlebars = require("handlebars");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+const path = require("path");
+const fs = require("fs");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+router.get("/", (req, res) => {
+  const date = new Date();
+  const filePath = path.join(__dirname, "../pages/emailTemplate.html");
+  const source = fs.readFileSync(filePath, "utf-8").toString();
+  const template = handlebars.compile(source);
+  const replacements = {
+    receiptNum: Math.floor(date.getTime() / 1000),
+    orgName: "Dummy Name",
+    donationDate: date.toDateString(),
+    name: "IDK",
+    donorEmail: "dummy@gmail.com",
+    amount: "$10",
+    donationType: "One Time",
+  };
+
+  const htmlMail = template(replacements);
+
+  const OAuth2_client = new OAuth2(
+    process.env.clientId,
+    process.env.clientSecret,
+    process.env.redirectURL
+  );
+
+  OAuth2_client.setCredentials({ refresh_token: process.env.refreshToken });
+
+  const accessToken = OAuth2_client.getAccessToken();
+
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.user,
+      clientId: process.env.clientId,
+      clientSecret: process.env.clientSecret,
+      refreshToken: process.env.refreshToken,
+      accessToken: accessToken,
+    },
+  });
+
+  const mail_options = {
+    from: `Bot Mailer <${process.env.user}>`,
+    to: "singh.agam0009@gmail.com",
+    subject: "Auto Message",
+    html: htmlMail,
+  };
+
+  transport.sendMail(mail_options, function (error, result) {
+    if (error) {
+      console.log("Error: ", error);
+    } else {
+      console.log("Success: ", result);
+    }
+    transport.close();
+  });
+  res.json("bye");
+});
+
+module.exports = router;
