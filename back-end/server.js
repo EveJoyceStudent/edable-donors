102ae8a9992af54e475ad2e9aef8ce4d31a4ca11
Agam-S <singh.agam0009@gmail.com>
2022-10-05 18:42:30 +1100

    api only responds to specific origins now

--- back-end/server.js
+++ back-end/server.js
@@ -11,7 +11,15 @@ dotenv.config();
 const mail = require("./routes/mail");
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://edable-donor.azurewebsites.net",
+      "https://edable-donor-test.azurewebsites.net",
+      "http://localhost:3000",
+    ],
+  })
+);
 app.use(express.json());
 app.use("/mail", mail);
 
